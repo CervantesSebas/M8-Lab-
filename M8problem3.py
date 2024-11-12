fc5d088f7d4d541c5ab21332cfa11aae975e3fbd
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-11-12 15:43:47 -0600

    Add files via upload

---
+++ M8problem3.py
@@ -0,0 +1,12 @@
+#Sebastian Cervantes 
+#November 12, 2024 
+#Write a function that takes a list and prints if the value 5 is in that list.
+import random 
+
+list1 = [random.randrange(12)for list1 in range(10)]
+print (list1)
+
+if 5 in list1:
+    print ("5 is in the list")
+else:
+    print("5 is not in the list")
