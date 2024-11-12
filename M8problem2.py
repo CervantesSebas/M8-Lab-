ca921563de63d5dc5c9ea4abad52a9e30b58afce
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-11-12 13:53:49 -0600

    Add files via upload

---
+++ M8problem2.py
@@ -0,0 +1,12 @@
+#Sebastian Cervantes 
+#November 12, 2024 
+#Takes two inputs and determines whether the sum is greater, less than, or equal to 10
+ans = int(input("First input: "))
+ans2 = int(input("Second input: "))
+num = (10)
+if ans + ans2 == num: 
+    print ("The sum of your inputs is equal to 10")
+elif ans + ans2 > num:
+    print ("The sum of your inputs is greater than 10")
+elif ans + ans2 < num:
+    print ("The sum of your inputs is less than 10")    
