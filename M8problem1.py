ca32a2db7c682ab5c1dc258669ad84a1bbeb3270
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-11-12 13:47:05 -0600

    Add files via upload

---
+++ M8problem1.py
@@ -0,0 +1,9 @@
+#Sebastian Cervantes 
+#November 12, 2024 
+#This functions determines if the users two inputs are equal
+use = int(input("Please enter first number: "))
+use2 = int(input("Please enter second number: "))
+if use == use2:
+    print ("Your numbers are equal")
+else:
+    print ("Your numbers are not equal")
